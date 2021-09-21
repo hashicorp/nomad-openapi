23eb198a633020f8f17e039d5228d88ce4fdc87d
James Rasell <jrasell@hashicorp.com>
2021-09-21 12:33:11 +0100

    v1: add regions endpoint abstraction functions.

---
+++ v1/regions_test.go
@@ -0,0 +1,22 @@
+package v1
+
+import (
+	"testing"
+
+	"github.com/hashicorp/nomad/command/agent"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRegions_GetRegions(t *testing.T) {
+	t.Parallel()
+	httpTest(t, nil, func(s *agent.TestAgent) {
+
+		// Make the HTTP request
+		testClient, err := NewTestClient(s)
+		require.NoError(t, err)
+
+		result, err := testClient.Regions().GetRegions(queryOpts.Ctx())
+		require.NoError(t, err)
+		require.Equal(t, []string{"global"}, *result)
+	})
+}
