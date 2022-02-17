100542dc224ff4eb2b92813e36789aa9c3b53fb7
Charlie Voiselle <464492+angrycub@users.noreply.github.com>
2022-02-17 16:17:29 -0500

    Refactor test for parallelism and to reduce port use.

--- v1/deployments_test.go
+++ v1/deployments_test.go
@@ -7,117 +7,117 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestGetDeployments(t *testing.T) {
-	httpTest(t, nil, func(s *agent.TestAgent) {
-		testClient, err := NewTestClient(s)
-		require.NoError(t, err)
-
-		result, meta, err := testClient.Deployments().Deployments(queryOpts.Ctx())
-		require.NoError(t, err)
-		require.NotNil(t, meta)
-		require.Len(t, *result, 0)
-		require.NotNil(t, result)
-	})
+func TestDeployments(t *testing.T) {
+	httpTests(t, nil,
+		APITestCase{"GetDeployments", testGetDeployments},
+		APITestCase{"GetDeployment", testGetDeployment},
+		APITestCase{"GetDeploymentAllocations", testGetDeploymentAllocations},
+		APITestCase{"PostDeploymentFail", testPostDeploymentFail},
+		APITestCase{"PostDeploymentPause", testPostDeploymentPause},
+		APITestCase{"PostDeploymentPromote1", testPostDeploymentPromote1},
+		APITestCase{"PostDeploymentPromote2", testPostDeploymentPromote2},
+		APITestCase{"PostDeploymentAllocationHealth1", testPostDeploymentAllocationHealth1},
+		APITestCase{"PostDeploymentAllocationHealth2", testPostDeploymentAllocationHealth2},
+		APITestCase{"PostDeploymentUnblock", testPostDeploymentUnblock},
+		APITestCase{"JobDeployment", testJobDeployment},
+		APITestCase{"JobDeployments", testJobDeployments},
+		APITestCase{"JobStable", testJobStable},
+		APITestCase{"JobScaleStatus", testJobScaleStatus},
+		APITestCase{"JobScaleTaskGroup", testJobScaleTaskGroup},
+	)
 }
 
-func TestGetDeployment(t *testing.T) {
-	httpTest(t, nil, func(s *agent.TestAgent) {
-		testClient, err := NewTestClient(s)
-		require.NoError(t, err)
+func testGetDeployments(t *testing.T, s *agent.TestAgent) {
+	testClient, err := NewTestClient(s)
+	require.NoError(t, err)
 
-		result, meta, err := testClient.Deployments().GetDeployment(queryOpts.Ctx(), "70638f62-5c19-193e-30d6-f9d6e689ab8e")
-		require.Error(t, err, "deployment not found")
-		require.Nil(t, meta)
-		require.Nil(t, result)
-	})
+	result, meta, err := testClient.Deployments().Deployments(queryOpts.Ctx())
+	require.NoError(t, err)
+	require.NotNil(t, meta)
+	require.Len(t, *result, 0)
+	require.NotNil(t, result)
 }
 
-func TestGetDeploymentAllocations(t *testing.T) {
-	httpTest(t, nil, func(s *agent.TestAgent) {
-		testClient, err := NewTestClient(s)
-		require.NoError(t, err)
+func testGetDeployment(t *testing.T, s *agent.TestAgent) {
+	testClient, err := NewTestClient(s)
+	require.NoError(t, err)
 
-		result, meta, err := testClient.Deployments().Allocations(queryOpts.Ctx(), "5456bd7a-9fc0-c0dd-6131-cbee77f57577")
-		require.NoError(t, err)
-		require.NotNil(t, meta)
-		require.Len(t, *result, 0)
-		require.NotNil(t, result)
-	})
+	result, meta, err := testClient.Deployments().GetDeployment(queryOpts.Ctx(), "70638f62-5c19-193e-30d6-f9d6e689ab8e")
+	require.Error(t, err, "deployment not found")
+	require.Nil(t, meta)
+	require.Nil(t, result)
 }
 
-func TestPostDeploymentFail(t *testing.T) {
-	httpTest(t, nil, func(s *agent.TestAgent) {
-		testClient, err := NewTestClient(s)
-		require.NoError(t, err)
+func testGetDeploymentAllocations(t *testing.T, s *agent.TestAgent) {
+	testClient, err := NewTestClient(s)
+	require.NoError(t, err)
 
-		result, err := testClient.Deployments().Fail(writeOpts.Ctx(), "5456bd7a-9fc0-c0dd-6131-cbee77f57577")
-		require.Error(t, err, "deployment not found")
-		require.Nil(t, result)
-	})
+	result, meta, err := testClient.Deployments().Allocations(queryOpts.Ctx(), "5456bd7a-9fc0-c0dd-6131-cbee77f57577")
+	require.NoError(t, err)
+	require.NotNil(t, meta)
+	require.Len(t, *result, 0)
+	require.NotNil(t, result)
 }
 
-func TestPostDeploymentPause(t *testing.T) {
-	httpTest(t, nil, func(s *agent.TestAgent) {
-		testClient, err := NewTestClient(s)
-		require.NoError(t, err)
+func testPostDeploymentFail(t *testing.T, s *agent.TestAgent) {
+	testClient, err := NewTestClient(s)
+	require.NoError(t, err)
 
-		result, err := testClient.Deployments().Pause(writeOpts.Ctx(), "5456bd7a-9fc0-c0dd-6131-cbee77f57577", true)
-		require.Error(t, err, "deployment not found")
-		require.Nil(t, result)
-	})
+	result, err := testClient.Deployments().Fail(writeOpts.Ctx(), "5456bd7a-9fc0-c0dd-6131-cbee77f57577")
+	require.Error(t, err, "deployment not found")
+	require.Nil(t, result)
 }
 
-func TestPostDeploymentPromote1(t *testing.T) {
-	httpTest(t, nil, func(s *agent.TestAgent) {
-		testClient, err := NewTestClient(s)
-		require.NoError(t, err)
+func testPostDeploymentPause(t *testing.T, s *agent.TestAgent) {
+	testClient, err := NewTestClient(s)
+	require.NoError(t, err)
 
-		result, err := testClient.Deployments().Promote(writeOpts.Ctx(), "5456bd7a-9fc0-c0dd-6131-cbee77f57577", true, nil)
-		require.Error(t, err, "deployment not found")
-		require.Nil(t, result)
-	})
+	result, err := testClient.Deployments().Pause(writeOpts.Ctx(), "5456bd7a-9fc0-c0dd-6131-cbee77f57577", true)
+	require.Error(t, err, "deployment not found")
+	require.Nil(t, result)
 }
 
-func TestPostDeploymentPromote2(t *testing.T) {
-	httpTest(t, nil, func(s *agent.TestAgent) {
-		testClient, err := NewTestClient(s)
-		require.NoError(t, err)
+func testPostDeploymentPromote1(t *testing.T, s *agent.TestAgent) {
+	testClient, err := NewTestClient(s)
+	require.NoError(t, err)
 
-		result, err := testClient.Deployments().Promote(writeOpts.Ctx(), "5456bd7a-9fc0-c0dd-6131-cbee77f57577", false, []string{"web", "api-server"})
-		require.Error(t, err, "deployment not found")
-		require.Nil(t, result)
-	})
+	result, err := testClient.Deployments().Promote(writeOpts.Ctx(), "5456bd7a-9fc0-c0dd-6131-cbee77f57577", true, nil)
+	require.Error(t, err, "deployment not found")
+	require.Nil(t, result)
 }
 
-func TestPostDeploymentAllocationHealth1(t *testing.T) {
-	httpTest(t, nil, func(s *agent.TestAgent) {
-		testClient, err := NewTestClient(s)
-		require.NoError(t, err)
+func testPostDeploymentPromote2(t *testing.T, s *agent.TestAgent) {
+	testClient, err := NewTestClient(s)
+	require.NoError(t, err)
 
-		result, err := testClient.Deployments().AllocationHealth(writeOpts.Ctx(), "5456bd7a-9fc0-c0dd-6131-cbee77f57577", nil, nil)
-		require.Error(t, err, "must specify at least one healthy/unhealthy allocation ID")
-		require.Nil(t, result)
-	})
+	result, err := testClient.Deployments().Promote(writeOpts.Ctx(), "5456bd7a-9fc0-c0dd-6131-cbee77f57577", false, []string{"web", "api-server"})
+	require.Error(t, err, "deployment not found")
+	require.Nil(t, result)
 }
 
-func TestPostDeploymentAllocationHealth2(t *testing.T) {
-	httpTest(t, nil, func(s *agent.TestAgent) {
-		testClient, err := NewTestClient(s)
-		require.NoError(t, err)
+func testPostDeploymentAllocationHealth1(t *testing.T, s *agent.TestAgent) {
+	testClient, err := NewTestClient(s)
+	require.NoError(t, err)
 
-		result, err := testClient.Deployments().AllocationHealth(writeOpts.Ctx(), "5456bd7a-9fc0-c0dd-6131-cbee77f57577", []string{"eb13bc8a-7300-56f3-14c0-d4ad115ec3f5", "6584dad8-7ae3-360f-3069-0b4309711cc1"}, nil)
-		require.Error(t, err, "deployment not found")
-		require.Nil(t, result)
-	})
+	result, err := testClient.Deployments().AllocationHealth(writeOpts.Ctx(), "5456bd7a-9fc0-c0dd-6131-cbee77f57577", nil, nil)
+	require.Error(t, err, "must specify at least one healthy/unhealthy allocation ID")
+	require.Nil(t, result)
 }
 
-func TestPostDeploymentUnblock(t *testing.T) {
-	httpTest(t, nil, func(s *agent.TestAgent) {
-		testClient, err := NewTestClient(s)
-		require.NoError(t, err)
+func testPostDeploymentAllocationHealth2(t *testing.T, s *agent.TestAgent) {
+	testClient, err := NewTestClient(s)
+	require.NoError(t, err)
 
-		result, err := testClient.Deployments().Unblock(writeOpts.Ctx(), "5456bd7a-9fc0-c0dd-6131-cbee77f57577")
-		require.Error(t, err, "deployment not found")
-		require.Nil(t, result)
-	})
+	result, err := testClient.Deployments().AllocationHealth(writeOpts.Ctx(), "5456bd7a-9fc0-c0dd-6131-cbee77f57577", []string{"eb13bc8a-7300-56f3-14c0-d4ad115ec3f5", "6584dad8-7ae3-360f-3069-0b4309711cc1"}, nil)
+	require.Error(t, err, "deployment not found")
+	require.Nil(t, result)
+}
+
+func testPostDeploymentUnblock(t *testing.T, s *agent.TestAgent) {
+	testClient, err := NewTestClient(s)
+	require.NoError(t, err)
+
+	result, err := testClient.Deployments().Unblock(writeOpts.Ctx(), "5456bd7a-9fc0-c0dd-6131-cbee77f57577")
+	require.Error(t, err, "deployment not found")
+	require.Nil(t, result)
 }
--- v1/status_test.go
+++ v1/status_test.go
@@ -7,27 +7,30 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestGetStatusLeader(t *testing.T) {
-	httpTest(t, nil, func(s *agent.TestAgent) {
-		testClient, err := NewTestClient(s)
-		require.NoError(t, err)
-
-		result, err := testClient.Status().Leader(queryOpts.Ctx())
-		require.NoError(t, err)
-		// using Contains because the port is random
-		require.Contains(t, *result, "127.0.0.1:")
-	})
+func TestStatus(t *testing.T) {
+	httpTests(t, nil,
+		APITestCase{"GetStatusLeaders", testGetStatusLeader},
+		APITestCase{"GetStatusPeers", testGetStatusPeers},
+	)
 }
 
-func TestGetStatusPeers(t *testing.T) {
-	httpTest(t, nil, func(s *agent.TestAgent) {
-		testClient, err := NewTestClient(s)
-		require.NoError(t, err)
+func testGetStatusLeader(t *testing.T, s *agent.TestAgent) {
+	testClient, err := NewTestClient(s)
+	require.NoError(t, err)
 
-		result, err := testClient.Status().Peers(queryOpts.Ctx())
-		require.NoError(t, err)
-		require.Len(t, *result, 1)
-		// using Contains because the port is random
-		require.Contains(t, (*result)[0], "127.0.0.1:")
-	})
+	result, err := testClient.Status().Leader(queryOpts.Ctx())
+	require.NoError(t, err)
+	// using Contains because the port is random
+	require.Contains(t, *result, "127.0.0.1:")
+}
+
+func testGetStatusPeers(t *testing.T, s *agent.TestAgent) {
+	testClient, err := NewTestClient(s)
+	require.NoError(t, err)
+
+	result, err := testClient.Status().Peers(queryOpts.Ctx())
+	require.NoError(t, err)
+	require.Len(t, *result, 1)
+	// using Contains because the port is random
+	require.Contains(t, (*result)[0], "127.0.0.1:")
 }
